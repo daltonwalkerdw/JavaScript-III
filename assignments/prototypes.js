85bab5c0507b911946b3388cfc54bca57dcbb745
daltonwalkerdw <53591219+daltonwalkerdw@users.noreply.github.com>
2019-09-10 18:48:35 -0400

    finished/strecth goal completed.

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -15,6 +15,17 @@
   * dimensions (These represent the character's size in the video game)
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
+function GameObject(gameobj){
+this.createdAt = gameobj.createdAt,
+this.name = gameobj.name,
+this.dimensions = gameobj.dimensions
+}
+
+GameObject.prototype.destroy = function () {
+  return `${this.name} was removed from the game.`;
+}
+
+
 
 /*
   === CharacterStats ===
@@ -22,6 +33,18 @@
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+function CharacterStats(charstats){
+  GameObject.call(this, charstats),
+  this.healthPoints = charstats.healthPoints
+  
+}
+
+
+CharacterStats.prototype = Object.create(GameObject.prototype);
+
+CharacterStats.prototype.takeDamage = function(){
+    return `${this.name} took damage`
+  }
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
@@ -32,7 +55,21 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
- 
+ function Humanoid(humantraits) {
+CharacterStats.call(this, humantraits)
+this.team = humantraits.team,
+this.weapons = humantraits.weapons,
+this.language = humantraits.language
+}
+
+Humanoid.prototype = Object.create(CharacterStats.prototype)
+
+Humanoid.prototype.greet = function(){
+  return `${this.name} greets you in ${this.language}`
+  };
+
+
+
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -41,22 +78,22 @@
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
-  const mage = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 1,
-      height: 1,
-    },
-    healthPoints: 5,
-    name: 'Bruce',
-    team: 'Mage Guild',
-    weapons: [
-      'Staff of Shamalama',
-    ],
-    language: 'Common Tongue',
-  });
+// /*
+const mage = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 1,
+    height: 1,
+  },
+  healthPoints: 5,
+  name: 'Bruce',
+  team: 'Mage Guild',
+  weapons: [
+    'Staff of Shamalama',
+  ],
+  language: 'Common Tongue',
+});
 
   const swordsman = new Humanoid({
     createdAt: new Date(),
@@ -102,9 +139,79 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
+  
+// */
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
   // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
   // * Create two new objects, one a villain and one a hero and fight it out with methods!
+
+  function Villain (villainattr){
+    Humanoid.call(this,villainattr ),
+    this.weapondamage = villainattr.weapondamage
+    this.attacks = villainattr.attacks
+    this.getshurt = function(){
+      return `${this.name} takes ${hero.weapondamage} damage, leaving it with ${this.healthPoints - hero.weapondamage} HP`
+    },
+    this.attacksback = function(){
+      return `${this.name} attacks back at extreme speed getting two hits in, leaving ${hero.name} at ${(hero.healthPoints - this.weapondamage) - this.weapondamage} HP`
+    }
+  }
+
+  Villain.prototype = Object.create(Humanoid.prototype)
+
+  const villain = new Villain({
+    createdAt: new Date(),
+    dimensions: {
+      length: 3,
+      width: 4,
+      height: 6,
+    },
+    healthPoints: 100,
+    name: 'Hellspawn',
+    team: 'Hell',
+    weapons: [
+      "Rancid Claws"
+    ],
+    weapondamage: 28,
+    language: 'Sign Language',
+  });
+
+  function Hero (heroattr){
+    Humanoid.call(this, heroattr),
+    this.weapondamage = heroattr.weapondamage
+    this.attacks = function(){
+      return `I, ${this.name} swing mighty ${this.weapons} to rid this world of ${villain.name}!`
+    },
+    this.finalmove = function(){
+    this.weapons.push(villain.weapons)  
+    return `${this.name} uses all the flesh absored into ${this.weapons[0]} to do a powerful strike! finsihing off ${villain.name}! ${this.name} has acquired ${villain.weapons}!`
+    }
+  }
+
+  Hero.prototype = Object.create(Humanoid.prototype)
+
+  const hero = new Hero({
+    createdAt: new Date(),
+    dimensions: {
+      length: 1,
+      width: 2,
+      height: 5,
+    },
+    healthPoints: 75,
+    name: 'Yacoatl',
+    team: 'Forest Kingdom',
+    weapons: [
+      "Fleshbane"
+    ],
+    weapondamage: 45,
+    language: 'Elvish',
+  });
+
+  console.log(hero.attacks())
+  console.log(villain.getshurt())
+  console.log(villain.attacksback())
+  console.log(hero.finalmove())
+  console.log(hero.weapons)
--- assignments/this.js
+++ assignments/this.js
@@ -1,15 +1,23 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding => Using "strict mode" to bypass window object of .this.
+* 2. Implicit binding => Makes use of .this keyword.
+* 3. New binding => Creates a new function based on a constructor, uses the "new" keyword.
+* 4. Explicit binding => Uses apply, call, and bind to point to .this.
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
+
+console.log(this);
+function goodbye (param) {
+  "use strict";
+  console.log(this);
+  return `I am going to say ${param}`;
+}
+console.log(goodbye('goodbye'));
 
 // code example for Window Binding
 
@@ -17,10 +25,43 @@
 
 // code example for Implicit Binding
 
+const dog = {
+    name:"Bacon",
+    age:6,
+    food:"Beef jurkey",
+    likesTo: function (){
+        return `${this.name} likes to eat ${this.food}`
+    },
+};
+
+console.log(dog.likesTo())
+
 // Principle 3
 
+function Fish(size){
+   this.size = size;
+   this.howBig = function(){
+     return `This fish is ${size}`
+   };
+}
+
+const goldfish = new Fish("small");
+
+console.log(goldfish.howBig())
 // code example for New Binding
 
 // Principle 4
+const myname = {
+    name: "Dalton"
+}; 
+
+let pets = ["Dog", "Bearded dragon"];
+
+function myanimals(pet, pet2){
+    "use strict";
+    return `My name is ${this.name} and i have a ${pet} and a ${pet2}`
+}
+
+console.log(myanimals.apply(myname, pets))
 
 // code example for Explicit Binding
